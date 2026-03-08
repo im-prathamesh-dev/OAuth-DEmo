688dec000be4530688e295f24cc40bd4e506180d
im-prathamesh-dev <prathamesh0755@gmail.com>
2026-03-08 13:32:18 +0530

    feat: add user session info API

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,11 @@
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.status(401).json({
+    message: "User not authenticated"
+  });
+}
+
+module.exports = isAuthenticated;
